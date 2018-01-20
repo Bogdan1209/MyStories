196866cfd6bdd9044728e5c36e597d694bde8f41
Bach <bogdan1209@rambler.ru>
2018-01-20 23:02:01 +0200

    Added EF, corrected gitignore

---
+++ MyStories.DAL/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyStories.DAL
+{
+    public class AppContext: DbContext
+    {
+        public AppContext(){
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyStories;uid=SA;pwd=Osinovka1;Trusted_Connection=True;");
+        }
+    }
+}
